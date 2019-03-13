cd6e8d943f200af02555ba5fc622948d4201ed74
Adam S <adam.sarli@gmail.com>
2019-03-13 16:41:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(int)
+  if int == 1 || int == -1 || int == 0 
+    return false 
+  elsif int < 0 
+    int = int * -1
+    puts int 
+  end 
+  range = (2..int-1).to_a
+  range.none? do |divisor|
+    int % divisor == 0 
+  end 
+end 
+
+puts prime?(-5)
