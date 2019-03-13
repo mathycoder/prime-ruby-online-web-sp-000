65fa550cbbb888b319f918aa0ede03e3b1fb709a
Adam S <adam.sarli@gmail.com>
2019-03-13 16:43:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,8 @@ def prime?(int)
   if int == 1 || int < 0  || int == 0 
     return false 
   end 
-  range = (2..int-1).to_a
-  range.none? do |divisor|
+  #range = (2..int-1).to_a
+  (2..int-1).to_a.none? do |divisor|
     int % divisor == 0 
   end 
 end 
